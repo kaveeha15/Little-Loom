0f9f1fa71bc8b55fe74e4319f8dc55ec592f9eca
kaveesha <nimanshikavisha@gmail.com>
2025-09-17 14:23:40 +0530

    addProducts & editProducts UI Created

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import SignIn from './pages/signIn'
 import SignUp from './pages/signUp'
 import Reviews from './pages/reviews'
 import OrderPlacement from './pages/orderPlacement'
+import AddProducts from './pages/admin/addProducts'
+import EditProducts from './pages/admin/editProducts'
 function App() {
  
   return (
     <div className="appContainer">
-      <OrderPlacement/>
+      <EditProducts/>
     </div>
     
     
---
+++ src/pages/admin/editProducts.jsx
@@ -0,0 +1,32 @@
+import '../../css/admin/addProducts.css'
+const EditProducts = () => {
+    return ( 
+<div className="addProductContainer">
+    <div className="addProductForm">
+          <h2>Edit Products</h2>
+        <div className="addImg">
+            <input type="file" name="addImg"  />
+        </div>
+        <h3>Change Product Image</h3>
+        <div className="addProductInput">
+            <label>Name : </label>
+            <input type="text" name="Name"  />
+            <label> Price : </label>
+            <input type="text" name="price"  />
+            <label>No  of Items : </label>
+            <input type="text" name="noItem" />
+            <label>Product Description :</label>
+            <textarea name="des"  />
+        </div>
+        <div className="addBtn">
+            <p>Save</p>
+        </div>
+
+    </div>
+  
+</div>
+
+     );
+}
+ 
+export default EditProducts;
